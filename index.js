1c22d0cf35d928a6af89e3556426a73d18bc3a8b
carlos <bar12033@byui.edu>
2018-11-21 22:33:04 -0600

    atempting to route

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/logIn'));
+
+
+/*router.post('/game/submit', function (req,res) {
+    var id = req.body.id;
+    res.redirect('/game/'+ id);
+});*/
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
