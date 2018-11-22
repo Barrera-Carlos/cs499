7cc31de6b69ffe7a7a4ef0539801194e02f62e04
carlos <bar12033@byui.edu>
2018-11-21 22:41:16 -0600

    adding npm

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/logIn'));
 
 
-router.post('/game/submit', function (req,res) {
+app.post('/game/submit', function (req,res) {
     var id = req.body.id;
     res.redirect('pages/game/'+ id);
 });
