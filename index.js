78306e1ecc4cd8475edd403f3c56b0c92aedd8cf
carlos <bar12033@byui.edu>
2018-11-21 23:26:54 -0600

    working on getting req.body to work

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
